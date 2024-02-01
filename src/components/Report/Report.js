de9e3efe16a70132363aa7ac7b490133a7a7b870
Ivan <ivan-kostyulin@yandex.ru>
2024-02-01 14:42:30 +0300

    fix: api url

--- src/components/Report/Report.js
+++ src/components/Report/Report.js
@@ -181,7 +181,6 @@ function Report() {
             </div>
           </Table>
         }
-        
         </>
       }       
       </>
--- src/utils/config.js
+++ src/utils/config.js
@@ -2,6 +2,6 @@ const currentUrl = window.location.href;
 
 const urlObject = new URL(currentUrl);
 
-export const API_URL = 'https://kst.emiit.ru' + '/api';
+export const API_URL = urlObject.origin + '/api';
 
 //export const API_URL = 'https://kst.emiit.ru/api';
